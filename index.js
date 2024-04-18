0612b68ca706cc576cef04bb45e338821efdb1e1
superwibr <superwibr@gmail.com>
2024-04-18 17:21:48 -0400

    homeball & cheeto experiment

--- index.js
+++ index.js
@@ -102,6 +102,55 @@ const state = {
                         );
                         snowball_iter++;
                     }, 50, snowball_count);
+
+                    // homeball pain
+                    loop(me, me => {
+                        const homeballs = [];
+                        let retreat = false;
+                        animate(me, (me, { here, canvas }) => homeballs.push(summon(
+                            "snowball",
+                            here,
+                            { x: canvas.width / 6, y: 0 },
+                            { vel: { x: 0, y: 4 } },
+                            me => loop(me, me => {
+                                if (retreat) return;
+                                faceEntity(me, player);
+                                velocityFacingAdd(me, 0.2);
+                            }, 10)
+                        )), 2, 32);
+
+                        wait(me, () => homeballs.forEach(me => {
+                            retreat = true;
+                            //me.vel.x = 0;
+                            //me.vel.y = 4;
+                            velocityFacing(me, 4)
+                        }), 100)
+                    }, 500);
+
+                    // cheeto alert
+                    loop(me, me => {
+                        const cheeto_parts = [];
+                        animate(me, (me, { here, canvas }) => cheeto_parts.push(summon(
+                            "cheeto_segment",
+                            here,
+                            { x: canvas.width / 6 * 5, y: 0 },
+                            { friction: .001 },
+                            me => { }
+                        )), 2, 32);
+
+                        loop(me, me => {
+                            const loc = { ...player.pos };
+                            cheeto_parts.forEach((me, i) => {
+                                if (i == 0) {
+                                    face(me, loc);
+                                    wait(me, me => velocityFacingAdd(me, 0.06), 100);
+                                } else {
+                                    const ploc = { ...cheeto_parts[i - 1].pos };
+                                    wait(me, me => me.pos = ploc, 0);
+                                }
+                            });
+                        }, 10)
+                    }, 800);
                 }),
 
                 // player
