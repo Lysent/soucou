2ed1a8f7ab44e64cfbba17908368ccb3b261b82d
superwibr <superwibr@gmail.com>
2024-05-21 21:15:40 -0400

    la creatura (cutscene)

--- cutscene/cutscene.js
+++ cutscene/cutscene.js
@@ -23,14 +23,14 @@ let currentIndex = 0;
 
 const titleElement = document.querySelector('.title');
 const textElement = document.querySelector('.text');
-//const imageElement = document.querySelector('.image');
+// const imageElement = document.querySelector('.image');
 
 document.addEventListener('click', function () {
 
     textElement.textContent = texts[currentIndex];
     titleElement.textContent = titles[currentIndex];
-    //imageElement.src = images[currentIndex].src;
-    //imageElement.alt = images[currentIndex].alt;
+    // imageElement.src = images[currentIndex].src;
+    // imageElement.alt = images[currentIndex].alt;
 
     currentIndex++;
     if (currentIndex >= texts.length) {
